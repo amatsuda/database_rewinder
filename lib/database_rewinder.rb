a1ed51332396fd4a9f59e9d41e8329d9e3bcb6ac
Akira Matsuda <ronnie@dio.jp>
2013-09-09 20:01:12 +0900

    Initial Implementation
    * a very simple implementation of database cleaner that is almost API compatible with database_cleaner
    * supports multiple databases
    * supports ActiveRecord only
    * cleans tables via DELETE SQL. No other strategies ATM

--- lib/database_rewinder.rb
+++ lib/database_rewinder.rb
@@ -1,3 +1,76 @@
+require_relative 'database_rewinder/cleaner'
+
 module DatabaseRewinder
   VERSION = Gem.loaded_specs['database_rewinder'].version.to_s
+
+  class << self
+    def init
+      @cleaners, @table_names_cache, @clean_all, @only, @except = [], {}, false
+      @db_config = YAML::load(ERB.new(IO.read(Rails.root.join 'config/database.yml')).result)
+    end
+
+    def create_cleaner(connection_name)
+      config = @db_config[connection_name] or raise %Q[Database configuration named "#{connection_name}" is not configured.]
+
+      Cleaner.new(db: config['database'], connection_name: connection_name, only: @only, except: @except).tap {|c| @cleaners << c}
+    end
+
+    def [](_orm, connection: nil, **)
+      if (cl = @cleaners.detect {|c| c.connection_name == connection})
+        return cl
+      end
+
+      create_cleaner connection
+    end
+
+    def all=(v)
+      @clean_all = v
+    end
+
+    def cleaners
+      create_cleaner 'test' if @cleaners.empty?
+      @cleaners
+    end
+
+    def record_inserted_table(connection, sql)
+      database = connection.instance_variable_get(:'@config')[:database]
+      cleaner = cleaners.detect {|c| c.db == database} or return
+
+      match = sql.match(/\AINSERT INTO [`"]?([^\s`"]+)[`"]?/)
+      table = match[1] if match
+      if table
+        cleaner.inserted_tables << table unless cleaner.inserted_tables.include? table
+        cleaner.pool ||= connection.pool
+      end
+    end
+
+    def clean
+      if @clean_all
+        clean_all
+      else
+        cleaners.each {|c| c.clean}
+      end
+    end
+
+    def clean_all
+      cleaners.each {|c| c.clean_all}
+    end
+
+    # for database_cleaner compat
+    def clean_with(*args)
+      cleaners.each {|c| c.clean_with *args}
+    end
+
+    # for database_cleaner compat
+    def start; end
+    def strategy=(_strategy, only: nil, except: nil, **)
+      @only, @except = only, except
+    end
+
+    # cache AR connection.tables
+    def all_table_names(connection)
+      db = connection.instance_variable_get(:'@config')[:database]
+      @table_names_cache[db] ||= connection.tables
+    end
+  end
 end
---
+++ lib/database_rewinder/cleaner.rb
@@ -0,0 +1,54 @@
+module DatabaseRewinder
+  class Cleaner
+    attr_accessor :db, :connection_name, :inserted_tables, :pool
+
+    def initialize(db: nil, connection_name: nil, only: nil, except: nil)
+      @db, @connection_name, @only, @except = db, connection_name, Array(only), Array(except)
+      reset
+    end
+
+    def clean
+      return unless pool
+      return if inserted_tables.empty?
+
+      delete_all (ar_conn = pool.connection), DatabaseRewinder.all_table_names(ar_conn) & inserted_tables
+      reset
+    end
+
+    def clean_all
+      return unless pool
+
+      delete_all (ar_conn = pool.connection), DatabaseRewinder.all_table_names(ar_conn)
+      reset
+    end
+
+    def clean_with(_strategy, only: nil, except: nil, **)
+      @only += Array(only) unless only.blank?
+      @except += Array(except) unless except.blank?
+      clean_all
+    end
+
+    # for database_cleaner compat
+    def strategy=(_strategy, only: nil, except: nil, **)
+      @only += Array(only) unless only.blank?
+      @except += Array(except) unless except.blank?
+    end
+
+    private
+    def delete_all(ar_conn, tables)
+      tables = tables & @only if @only.any?
+      tables -= @except if @except.any?
+      return if tables.empty?
+
+      ar_conn.disable_referential_integrity do
+        tables.each do |table_name|
+          ar_conn.execute "DELETE FROM #{ar_conn.quote_table_name(table_name)};"
+        end
+      end
+    end
+
+    def reset
+      @inserted_tables = []
+    end
+  end
+end
