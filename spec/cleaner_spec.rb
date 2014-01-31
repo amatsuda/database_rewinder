ff8e7c945f9989cf07b2fa2d99811b90d4c9efb2
Akira Matsuda <ronnie@dio.jp>
2014-02-01 04:18:08 +0900

    Indent :arrow_right::arrow_right:

--- spec/cleaner_spec.rb
+++ spec/cleaner_spec.rb
@@ -1,47 +1,45 @@
 require 'spec_helper'
 
 module DatabaseRewinder
+  describe Cleaner do
+    describe '#strategy=' do
+      before { @cleaner = described_class.new }
 
-describe Cleaner do
-  describe '#strategy=' do
-    before { @cleaner = described_class.new }
+      context 'when only strategy is given' do
+        before { @cleaner.strategy = :truncation }
 
-    context 'when only strategy is given' do
-      before { @cleaner.strategy = :truncation }
-
-      it 'should ignore strategy' do
-        expect(@cleaner.instance_variable_get(:@only)).to eq([])
-        expect(@cleaner.instance_variable_get(:@except)).to eq([])
+        it 'should ignore strategy' do
+          expect(@cleaner.instance_variable_get(:@only)).to eq([])
+          expect(@cleaner.instance_variable_get(:@except)).to eq([])
+        end
       end
-    end
 
-    context 'when only option is given' do
-      before { @cleaner.strategy = :truncation, { only: ['foos'] } }
+      context 'when only option is given' do
+        before { @cleaner.strategy = :truncation, { only: ['foos'] } }
 
-      it 'should set only option' do
-        expect(@cleaner.instance_variable_get(:@only)).to eq(['foos'])
-        expect(@cleaner.instance_variable_get(:@except)).to eq([])
+        it 'should set only option' do
+          expect(@cleaner.instance_variable_get(:@only)).to eq(['foos'])
+          expect(@cleaner.instance_variable_get(:@except)).to eq([])
+        end
       end
-    end
 
-    context 'when except option is given' do
-      before { @cleaner.strategy = :truncation, { except: ['foos'] } }
+      context 'when except option is given' do
+        before { @cleaner.strategy = :truncation, { except: ['foos'] } }
 
-      it 'should set only option' do
-        expect(@cleaner.instance_variable_get(:@only)).to eq([])
-        expect(@cleaner.instance_variable_get(:@except)).to eq(['foos'])
+        it 'should set only option' do
+          expect(@cleaner.instance_variable_get(:@only)).to eq([])
+          expect(@cleaner.instance_variable_get(:@except)).to eq(['foos'])
+        end
       end
-    end
 
-    context 'when only and except option are given' do
-      before { @cleaner.strategy = :truncation, { only: ['foos'], except: ['bars'] } }
+      context 'when only and except option are given' do
+        before { @cleaner.strategy = :truncation, { only: ['foos'], except: ['bars'] } }
 
-      it 'should set only option' do
-        expect(@cleaner.instance_variable_get(:@only)).to eq(['foos'])
-        expect(@cleaner.instance_variable_get(:@except)).to eq(['bars'])
+        it 'should set only option' do
+          expect(@cleaner.instance_variable_get(:@only)).to eq(['foos'])
+          expect(@cleaner.instance_variable_get(:@except)).to eq(['bars'])
+        end
       end
     end
   end
 end
-
-end # module DatabaseRewinder
