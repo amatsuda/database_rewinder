5e80c21b9bf51d61ded5b738692aa9e480cb5845
deeeki <deeeki@gmail.com>
2016-05-29 03:06:44 +0900

    Reduce line and indent

--- spec/cleaner_spec.rb
+++ spec/cleaner_spec.rb
@@ -1,35 +1,33 @@
 require 'spec_helper'
 
-module DatabaseRewinder
-  describe Cleaner do
-    describe '#strategy=' do
-      before { @cleaner = described_class.new(only: ['foos'], except: 'bars') }
+describe DatabaseRewinder::Cleaner do
+  describe '#strategy=' do
+    before { @cleaner = described_class.new(only: ['foos'], except: 'bars') }
 
-      context 'without options' do
-        before { @cleaner.strategy = :truncation }
+    context 'without options' do
+      before { @cleaner.strategy = :truncation }
 
-        it 'should keep instance variables' do
-          expect(@cleaner.instance_variable_get(:@only)).to eq(['foos'])
-          expect(@cleaner.instance_variable_get(:@except)).to eq(['bars'])
-        end
+      it 'should keep instance variables' do
+        expect(@cleaner.instance_variable_get(:@only)).to eq(['foos'])
+        expect(@cleaner.instance_variable_get(:@except)).to eq(['bars'])
       end
+    end
 
-      context 'with options (an array or a string)' do
-        before { @cleaner.strategy = :truncation, { only: ['bars'], except: 'bazs' } }
+    context 'with options (an array or a string)' do
+      before { @cleaner.strategy = :truncation, { only: ['bars'], except: 'bazs' } }
 
-        it 'should overwrite instance variables' do
-          expect(@cleaner.instance_variable_get(:@only)).to eq(['bars'])
-          expect(@cleaner.instance_variable_get(:@except)).to eq(['bazs'])
-        end
+      it 'should overwrite instance variables' do
+        expect(@cleaner.instance_variable_get(:@only)).to eq(['bars'])
+        expect(@cleaner.instance_variable_get(:@except)).to eq(['bazs'])
       end
+    end
 
-      context 'with options (an empty array or nil)' do
-        before { @cleaner.strategy = :truncation, { only: [], except: nil } }
+    context 'with options (an empty array or nil)' do
+      before { @cleaner.strategy = :truncation, { only: [], except: nil } }
 
-        it 'should overwrite instance variables even if they are empty/nil' do
-          expect(@cleaner.instance_variable_get(:@only)).to eq([])
-          expect(@cleaner.instance_variable_get(:@except)).to eq([])
-        end
+      it 'should overwrite instance variables even if they are empty/nil' do
+        expect(@cleaner.instance_variable_get(:@only)).to eq([])
+        expect(@cleaner.instance_variable_get(:@except)).to eq([])
       end
     end
   end
