70e3e599a8f16fca8c32546f7903f8ff51ea616d
Akira Matsuda <ronnie@dio.jp>
2016-11-10 00:46:25 +0900

    Always create new DB for testing

--- test/fake_app.rb
+++ test/fake_app.rb
@@ -2,6 +2,7 @@
 
 ENV['DB'] ||= 'sqlite3'
 require 'active_record/railtie'
+load 'active_record/railties/databases.rake'
 
 module DatabaseRewinderTestApp
   Application = Class.new(Rails::Application) do
@@ -13,6 +14,11 @@ module DatabaseRewinderTestApp
   end.initialize!
 end
 
+require 'active_record/base'
+ActiveRecord::Tasks::DatabaseTasks.drop_current ENV['DB']
+ActiveRecord::Tasks::DatabaseTasks.drop_current "#{ENV['DB']}_2"
+ActiveRecord::Tasks::DatabaseTasks.create_current ENV['DB']
+ActiveRecord::Tasks::DatabaseTasks.create_current "#{ENV['DB']}_2"
 
 # models
 class Foo < ActiveRecord::Base; end
@@ -25,6 +31,7 @@ end
 # migrations
 class CreateAllTables < ActiveRecord::Migration
   def self.up
+    ActiveRecord::Base.establish_connection ENV['DB'].to_sym
     create_table(:foos) {|t| t.string :name }
     create_table(:bars) {|t| t.string :name }
     create_table(:bazs) {|t| t.string :name }
