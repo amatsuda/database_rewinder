e42b20b059f76709848a658828a763717b03189a
Akira Matsuda <ronnie@dio.jp>
2023-11-15 18:33:14 +0900

    Maybe it's time for a piece of meta programming?
    It turned out that we need to handle non-kwargs version of internal_exec_query as well, and I don't want to copy-paste the same method body anymore...

--- lib/database_rewinder/active_record_monkey.rb
+++ lib/database_rewinder/active_record_monkey.rb
@@ -2,62 +2,27 @@
 
 module DatabaseRewinder
   module InsertRecorder
-    module Execute
-      module NoKwargs
-        def execute(sql, *)
-          DatabaseRewinder.record_inserted_table self, sql
-          super
-        end
-      end
-
-      module WithKwargs
-        def execute(sql, *, **)
-          DatabaseRewinder.record_inserted_table self, sql
-          super
-        end
-
-        def raw_execute(sql, *, **)
-          DatabaseRewinder.record_inserted_table self, sql
-          super
-        end
-      end
-    end
-
-    module ExecQuery
-      module NoKwargs
-        def exec_query(sql, *)
-          DatabaseRewinder.record_inserted_table self, sql
-          super
-        end
-      end
-
-      module WithKwargs
-        def exec_query(sql, *, **)
-          DatabaseRewinder.record_inserted_table self, sql
-          super
-        end
-
-        def internal_exec_query(sql, *, **)
-          DatabaseRewinder.record_inserted_table self, sql
-          super
-        end
-      end
-    end
-
     # This method actually no longer has to be a `prepended` hook because InsertRecorder is a module without a direct method now, but still doing this just for compatibility
     def self.prepended(mod)
-      if meth = mod.instance_method(:execute)
-        if meth.parameters.any? {|type, _name| [:key, :keyreq, :keyrest].include? type }
-          mod.send :prepend, Execute::WithKwargs
-        else
-          mod.send :prepend, Execute::NoKwargs
-        end
-      end
-      if meth = mod.instance_method(:exec_query)
-        if meth.parameters.any? {|type, _name| [:key, :keyreq, :keyrest].include? type }
-          mod.send :prepend, ExecQuery::WithKwargs
-        else
-          mod.send :prepend, ExecQuery::NoKwargs
+      [:execute, :exec_query, :internal_exec_query].each do |method_name|
+        if (meth = mod.instance_method(method_name))
+          method_body = if meth.parameters.any? {|type, _name| [:key, :keyreq, :keyrest].include? type }
+            <<-RUBY
+              def #{method_name}(sql, *, **)
+                DatabaseRewinder.record_inserted_table self, sql
+                super
+              end
+            RUBY
+          else
+            <<-RUBY
+              def #{method_name}(sql, *)
+                DatabaseRewinder.record_inserted_table self, sql
+                super
+              end
+            RUBY
+          end
+
+          mod.send :prepend, Module.new { class_eval method_body }
         end
       end
     end
