29fe702d691f589d9e1ef13c6fab57fc2b5975d6
Akira Matsuda <ronnie@dio.jp>
2025-01-17 01:13:04 +0900

    require logger before requiring rails
    require 'rails' fails with the following error on Rails 6
    > uninitialized constant ActiveSupport::LoggerThreadSafeLevel::Logger (NameError)

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,6 +5,8 @@ ENV['RAILS_ENV'] ||= 'test'
 $LOAD_PATH.unshift(File.join(__dir__, '..', 'lib'))
 $LOAD_PATH.unshift(__dir__)
 
+# require logger before rails or Rails 6 fails to boot
+require 'logger'
 require 'rails'
 require 'active_record'
 require 'database_rewinder'
